b3cb1763dddbb34e9034a1c544bb84cd5f58eae1
Hyunsu <ksoksl0711@gmail.com>
2021-12-04 14:08:45 -0800

    modify spaces

--- react-version/src/Navigation.js
+++ react-version/src/Navigation.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
-function Navigation () {
+function Navigation(){
   return (
-<nav>
-  <span class="nav--text">tweeter</span>
-  <div class="btn btn--scroll">
-    <a href="#" class="btn__text btn--borderless" title="Compose Tweet">
-      <span class="btn--bold">Write</span> a new tweet
-      <br/>
-      <i class="btn__icon fas fa-angle-double-down"></i>
-    </a>
-  </div>
-</nav>
+    <nav>
+      <span class="nav--text">tweeter</span>
+      <div class="btn btn--scroll">
+        <a href="#" class="btn__text btn--borderless" title="Compose Tweet">
+          <span class="btn--bold">Write</span> a new tweet
+          <br/>
+          <i class="btn__icon fas fa-angle-double-down"></i>
+        </a>
+      </div>
+    </nav>
   );
 }
 
